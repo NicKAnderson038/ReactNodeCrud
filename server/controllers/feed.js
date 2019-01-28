23ca0396fe8c06eb944b2a9b3ec5051cae58462c
NicKAnderson038 <andersonnick038@gmail.com>
2019-01-27 22:19:43 -0800

    first commit

---
+++ server/controllers/feed.js
@@ -0,0 +1,34 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      { 
+        _id: '1',
+        title: 'First Post', 
+        content: 'This is the first post!', 
+        imageUrl: 'images/meowing.jpg',
+        creator:{
+          name: 'Nick'
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: { 
+      _id: new Date().toISOString(), 
+      title, 
+      content,
+      creator:{
+        name: 'Nick'
+      },
+      createdAt: new Date()
+    }
+  });
+};
